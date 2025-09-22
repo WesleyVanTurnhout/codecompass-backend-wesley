691b2f7bbd0ced355303ed05053d9c8af3a4f443
wesley <wesley_turnhout28@hotmail.com>
2025-09-22 16:11:16 +0200

    Update ProfileDTO to include Id. (needed at me page)

--- src/main/java/org/example/codecompasssolo/dto/ProfileDTO.java
+++ src/main/java/org/example/codecompasssolo/dto/ProfileDTO.java
@@ -1,11 +1,13 @@
 package org.example.codecompasssolo.dto;
 
-
+import java.util.UUID;
 
 public class ProfileDTO {
+    private UUID id;
     private String displayName;
     private String role;
 
+    public UUID getId() { return id; }
 
     public String getDisplayName() {
         return displayName;
